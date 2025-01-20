ac3f2a089445a54faf413f204b77175918792448
Jabbar Jatoi <jabbar.jatoi99@gmail.com>
2025-01-20 11:52:04 +0500

    Add files via upload
    Here is assignment table of 2 through for loop

---
+++ Table.py
@@ -0,0 +1,16 @@
+x = 5
+for i in range(1, 11):
+    print(x, "x", i, "=", x*i)
+
+
+num: int = 2
+print("2 x 1 =", num * 1)
+print("2 x 2 =", num * 2)
+print("2 x 3 =", num * 3)
+print("2 x 4 =", num * 4)
+print("2 x 5 =", num * 5)
+print("2 x 6 =", num * 6)
+print("2 x 7 =", num * 7)
+print("2 x 8 =", num * 8)
+print("2 x 9 =", num * 9)
+print("2 x 10 =", num * 10)

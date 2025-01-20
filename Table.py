2b97480da77026946f7b810e99e22da2d410a3f3
Jabbar Jatoi <jabbar.jatoi99@gmail.com>
2025-01-20 12:02:34 +0500

    Update Table.py

--- Table.py
+++ Table.py
@@ -1,7 +1,21 @@
+# Assignment (Table of 2)
+# first option
+
 x = 5
 for i in range(1, 11):
     print(x, "x", i, "=", x*i)
 
+
+# Assignment (Table of 2)
+# second option F string
+
+x = 2
+for i in range(1,11):
+    print(f"{x} x {i} = {i*2} ")
+
+
+# Assignment (Table of 2)
+# sinmple table by perators
 
 num: int = 2
 print("2 x 1 =", num * 1)

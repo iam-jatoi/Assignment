a147ffccfdf13bf3dd7558c55137400a1a7119f9
Jabbar Jatoi <jabbar.jatoi99@gmail.com>
2025-01-24 15:21:41 +0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -4,20 +4,22 @@ print("1. Add (+)")
 print("2. Subtract (-)")
 print("3. Multiple (*)")
 print("4. Divide (/)")
-choice = input("Enter the choice: ")
+choice = input("Enter the choice by using Number: ")
 num2 = float(input("Enter the second number: "))
 print("Here is your answer")
 
-
 if choice == '1':
-    print(num1, "+", num2, "=", num1 + num2)
+    print(f"{num1} + {num2} = {num1 + num2}")
+    print("Thank You for Using Calculator")
 elif choice == '2':
-    print(num1, "-", num2, "=", num1 - num2)
+    print(f"{num1} - {num2} = {num1 - num2}")
+    print("Thank You for Using Calculator")
 elif choice == '3':
-    print(num1, "x", num2, "=", num1 * num2)
+    print(f"{num1} * {num2} = {num1 * num2}")
+    print("Thank You for Using Calculator")
 elif choice == '4':
-        print(num1, "/", num2, "=", num1 / num2)
+        print(f"{num1} / {num2} = {num1 / num2}")
+        print("Thank You for Using Calculator")
 else:
         print("Invalid input")
-
-print("Thank you for using Calculator!")
+        
